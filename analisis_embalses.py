82ea1518a0542f5b091a40176415f8c0c545b97d
velasquezmateo <mateov55@hotmail.com>
2026-03-05 10:14:54 -0500

    Grafico volumen útil terminado

--- analisis_embalses.py
+++ analisis_embalses.py
@@ -21,21 +21,34 @@ caudal_med_histo=pd.read_sql('SELECT * FROM embalses.caudal_med_hist',engine)
 
 volumen_util['date']=pd.to_datetime(volumen_util['date'],yearfirst=True)
 vol_actual=volumen_util[volumen_util['date']=='2026-03-03'].sort_values(by='value', ascending=False)
+vol_actual['value']=vol_actual['value']*100
+vol_actual['name']=vol_actual['name'].str.replace('AGREGADO BOGOTA', 'AGR. BOGOTA')
+
+#Establecer umbral de capacidad
+colors=['#d32f2f' if x >=95 else '#f57c00' if x >=85 else '#1976d2' for x in vol_actual['value']]
 
 #Gráfico volumen útil actual
-ax=sns.barplot(vol_actual,x='name',y='value')
-for c in ax.containers:
-    labels=ax.bar_label(c,
+sns.set_theme(style="white")
+fig, ax = plt.subplots(figsize=(12, 8))
+bar=sns.barplot(vol_actual,x='name',y='value',palette=colors)
+
+for c in bar.containers:
+    labels=bar.bar_label(c,
                         fmt='%.2f%%',
-                 label_type='edge',
-                 padding=3)
+                        label_type='edge',
+                        padding=3)
     for l in labels:
         l.set_rotation(45)
-plt.title('Porcentaje de volumen útil diario reportado al SIN')
-plt.tight_layout()
+
+ax.set_title('Porcentaje de volumen útil diario reportado al SIN',fontsize=18,fontweight='bold')
+plt.axhline(95,linestyle='--',color='black',linewidth=1,alpha=0.25,label='Umbral Crítico (95%)')
+plt.axhline(85,linestyle='--',color='black',linewidth=1,alpha=0.25,label='Alerta (85%)')
+plt.legend(loc='upper center',shadow=True)
 plt.xticks(rotation=45)
-plt.xlabel('Embalses')
-plt.ylabel('Volumen útil diario (%)')
-plt.ylim(0,1.1)
+plt.ylabel('Volumen útil diario (%)',fontweight='bold')
+plt.xlabel('Embalses', fontweight='bold')
+plt.ylim(0,110)
+sns.despine()
+plt.tight_layout()
 plt.show()
 
